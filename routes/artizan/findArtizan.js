f346ea8162c39ca92ce5e55179a5a05b44a4314a
Terry Amagboro <rukkiecodes@gmail.com>
2022-08-01 22:21:04 +0100

    find artizan

--- routes/artizan/findArtizan.js
+++ routes/artizan/findArtizan.js
@@ -1,25 +1,18 @@
 const router = require('express').Router()
 const User = require('../../models/user')
 
-// router.post('/getArtizan', async (req, res) => {
-//   const { name, email, phone, gender, state, lga, specialty } = req.body
+router.post('/getArtizan', async (req, res) => {
+  const { name, email, phone, gender, state, lga, specialty } = req.body
 
-//   try {
-//     const users = await User.find({ $or: [{ email }, { name }, { phone }, { gender }, { state }, { lga }, { specialty }] })
+  try {
+    const users = await User.find({ $or: [{ email }, { name }, { phone }, { gender }, { state }, { lga }, { specialty }] })
 
-//     return res.status(200).json({
-//       users
-//     })
-//   } catch (error) {
-//     throw error
-//   }
-// })
-
-router.get('/get', (req, res) => {
-  res.status(200).json({
-    text: 'hey',
-    message: 'the app works'
-  })
+    return res.status(200).json({
+      users
+    })
+  } catch (error) {
+    throw error
+  }
 })
 
 module.exports = router
