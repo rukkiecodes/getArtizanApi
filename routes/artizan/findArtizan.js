cea62729c6c504808bd5ee4af3a95aa47ccde8d1
Terry Amagboro <rukkiecodes@gmail.com>
2022-08-16 21:51:39 +0100

    find

--- routes/artizan/findArtizan.js
+++ routes/artizan/findArtizan.js
@@ -4,15 +4,10 @@ const User = require('../../models/user')
 router.post('/getArtizan', async (req, res) => {
   const { state, specialty } = req.body
 
-  const _state = state?.toLowerCase()
-  const _specialty = specialty?.toLowerCase()
-
   try {
-    const users = await User.find({ $or: [{ state: _state }, { specialty: _specialty }] })
+    const users = await User.find({ $and: [{ state }, { specialty }] })
 
-    return res.status(200).json({
-      users
-    })
+    return res.status(200).json({ users })
   } catch (error) {
     throw error
   }
