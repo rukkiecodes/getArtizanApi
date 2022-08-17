cb778fef038ec239f7f6f528804a9cd267cf50fb
Terry Amagboro <rukkiecodes@gmail.com>
2022-08-17 04:47:27 +0100

    send OTP

--- routes/auth/resendVerification.js
+++ routes/auth/resendVerification.js
@@ -61,12 +61,10 @@ const sendOTPVerificationEmail = async ({ _id, email, firstName }, res) => {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>${firstName} feedback</title>
-      </head>
-      <body
+        <title>OTP<body
         style="background-color: white; display: flex; justify-content: center; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif">
         <div style="width: 600px; max-width: 100%; background-color: white;">
-          <h1>Hello ${firstName}, Please verify your email</h1>
+          <h1>Hello, Please verify your email</h1>
           <p style="font-size: 2rem"><b>${otp}</b></p>
           <p>This code <b>expires in 1 hour</b></p>
         </div>
